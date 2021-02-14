034a7e14b82ca2f8b968ab5fe4794d7697c2b0bb
Lucero Amaolo <michilnx@gmail.com>
2021-02-14 19:22:52 -0300

    Only API

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import { useContext, useEffect } from "react";
 import Head from "next/head";
 import fetch from "isomorphic-fetch";
 import { CartContext } from "../GlobalState";
-
+/*
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import Products from "../components/Products";
-import Contact from "../components/Contact";
+import Contact from "../components/Contact";*/
 /*
 export const getStaticProps = async (context) => {
   const response = await fetch(`${process.env.URL}/api/products`);
@@ -19,28 +19,16 @@ export const getStaticProps = async (context) => {
   };
 };
 */
-export default function Home({ data }) {
-  const [state, dispatch] = useContext(CartContext);
-
-  useEffect(() => {
-    if (data) {
-      dispatch({
-        type: "ADD_PRODUCTS",
-        payload: {
-          products: data.products,
-        },
-      });
-    }
-  }, [data]);
-
+export default function Home() {
   return (
-    <Layout>
+    <>
       <Head>
         <title>Pasteleria Next.js</title>
       </Head>
-      <Hero />
-      <Products products={data.products} />
-      <Contact />
-    </Layout>
+      /<h1>Hola</h1>
+      {/* <Hero />
+      <Products products={data?.products} />
+      <Contact /> */}
+    </>
   );
 }
